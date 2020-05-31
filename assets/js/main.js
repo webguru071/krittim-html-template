9e25af49d8019c991a3a6c480bf5cb44b1d6dfd3
Hedayet@AndIT <webguru071@gmail.com>
2020-05-31 10:34:08 +0600

    project first commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).on('ready', function() {
+
+        // Header Sticky
+        $(window).on('scroll', function() {
+            if ($(this).scrollTop() > 120) {
+
+                $('.navbar-area-three').addClass("is-sticky-three");
+                $('.navbar-area').addClass("is-sticky");
+            } else {
+                $('.navbar-area-three').removeClass("is-sticky-three");
+                $('.navbar-area').removeClass("is-sticky");
+            }
+        });
+
+        // Mean Menu
+        jQuery('.mean-menu').meanmenu({
+            meanScreenWidth: "991"
+        });
+
+    });
+   
+    // Recent-work
+    $('.recent-wrapper').owlCarousel({
+        loop: true,
+        dots: false,
+        autoplayHoverPause: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 0,
+        nav: true,
+        navText: [
+
+            "<i class='flaticon-left'></i>",
+            "<i class='flaticon-right'></i>"
+
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 1,
+            },
+            1200: {
+                items: 1,
+            }
+        }
+    });
+
+
+}(jQuery));
+
+
